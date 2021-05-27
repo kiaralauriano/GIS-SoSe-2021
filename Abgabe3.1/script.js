cab7af0d3080dcf149cac42dc933480b9387fe49
kiaralauriano <kiaralauriano@icloud.com>
2021-05-27 18:18:46 +0200

    aufgabe2

--- Abgabe3.1/script.js
+++ Abgabe3.1/script.js
@@ -1,2 +1,17 @@
 "use strict";
+var Aufgabe3_1;
+(function (Aufgabe3_1) {
+    let formData = new FormData(document.forms[0]);
+    let btSend = document.getElementById("send");
+    btSend.addEventListener("click", send);
+    async function send() {
+        let url = "https://kiaralauriano.herokuapp.com/";
+        let query = new URLSearchParams(formData);
+        url = url + "?" + query.toString();
+        let response = await fetch(url);
+        let text = await response.text();
+        console.log("Answer:");
+        console.log(text);
+    }
+})(Aufgabe3_1 || (Aufgabe3_1 = {}));
 //# sourceMappingURL=script.js.map
--- Abgabe3.1/script.ts
+++ Abgabe3.1/script.ts
@@ -1,3 +1,16 @@
 namespace Aufgabe3_1 {
+    let formData: FormData = new FormData(document.forms[0]);
 
+    let btSend: HTMLButtonElement = <HTMLButtonElement>document.getElementById("send");
+    btSend.addEventListener("click", send);
+
+    async function send(): Promise<void> {
+        let url: string = "https://kiaralauriano.herokuapp.com/";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + query.toString();
+        let response: Response = await fetch(url);
+        let text: string = await response.text();
+        console.log("Answer:");
+        console.log(text);
+    }
 }
